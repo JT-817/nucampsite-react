5f340b422429856b7e98dc20cf12471aac121088
Josh Thompson <pvmphenom@gmail.com>
2022-11-01 19:15:17 -0400

    homepage, routing, directory page

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React from "react";
 import CampsiteCard from "./CampsiteCard";
-import { Col, Row } from 'reactstrap';
-import { CAMPSITES } from "../../app/shared/CAMPSITES";
+import { Col, Row } from "reactstrap";
+import { selectAllCampsites } from "./campsitesSlice";
 
-const CampsitesList = () => {
+const CampsitesList = ({ setCampsiteId }) => {
+  const campsites = selectAllCampsites();
+
   return (
-    <Row className='ms-auto'>
-        {CAMPSITES.map((campsite) => {
-            return(
-            <Col md='5'className='m-4' key={campsite.id}>
-                <CampsiteCard campsite={campsite} />
-            </Col>)
-        })}
+    <Row className="ms-auto">
+      {campsites.map((campsite) => {
+        return (
+          <Col
+            md="5"
+            className="m-4"
+            key={campsite.id}
+            onClick={() => setCampsiteId(campsite.id)}
+          >
+            <CampsiteCard campsite={campsite} />
+          </Col>
+        );
+      })}
     </Row>
-  )
-}
+  );
+};
 
-export default CampsitesList
+export default CampsitesList;
---
+++ src/features/display/DisplayList.js
@@ -0,0 +1,21 @@
+import { Col, Row } from 'reactstrap';
+import DisplayCard from './DisplayCard';
+import { selectFeaturedCampsites } from '../campsites/campsitesSlice';
+import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
+
+const DisplayList = () => {
+    const items = [selectFeaturedCampsites(), selectFeaturedPromotion()]
+  return (
+    <Row>
+        {items.map((item, idx) => {
+            return(
+                <Col md className='m-1' key={idx}>
+                    <DisplayCard item={item} />
+                </Col>
+            )
+        })}
+    </Row>
+)
+}
+
+export default DisplayList
