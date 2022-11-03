0d428a473cebd6a0741246f2ac18c548eb2ec12c
Josh Thompson <pvmphenom@gmail.com>
2022-11-03 19:53:43 -0400

    implementing campsite detail pages still buggy

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,15 +1,17 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
 const CampsiteCard = ({ campsite }) => {
-  const { image, name } = campsite;
+  const { id, image, name } = campsite;
   return (
+    <Link to={`${id}`}>
     <Card>
       <CardImg width='100%' src={image} alt={name} />
       <CardImgOverlay>
         <CardTitle>{name}</CardTitle>
       </CardImgOverlay>
     </Card>
+    </Link>
   );
 };
 
